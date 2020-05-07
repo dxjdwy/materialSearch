78026e23441e4877c0d8b1fdde898820ac019d27
dxj <dxjdwy@163.com>
2020-05-07 23:20:02 +0800

    springboot+access

---
+++ materialsearch/src/main/java/com/casic/materialsearch/MaterialsearchApplication.java
@@ -0,0 +1,13 @@
+package com.casic.materialsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialsearchApplication.class, args);
+    }
+
+}
